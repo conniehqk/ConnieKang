7271d0a0abdb5b1f51390ae7546685fc7900a547
conniekang <hqk1121@gmail.com>
2021-11-21 15:55:08 -0500

    getting ready to deploy?

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Menu, Container, Sidebar } from 'semantic-ui-react'
+import { Menu, Container } from 'semantic-ui-react'
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './Home';
 import About from './Aboutme';
@@ -15,9 +15,7 @@ function App() {
         <Menu size='huge' pointing secondary id="menu">
             <Menu.Item id="brand" as={Link} to='/' name="" onClick={handleItemClick} header>Connie Kang</Menu.Item>
             <Menu.Menu  position='right'>
-              <Sidebar only='mobile' toggleBtn={true} />
               <Menu.Item
-                only='computer'
                 name='about me'
                 as={Link} to='/about'
                 active={activeItem === 'about me'}
@@ -25,19 +23,16 @@ function App() {
               />
               <Menu.Item
                 name='project'
-                only='computer'
                 active={activeItem === 'project'}
                 onClick={handleItemClick}
               />
               <Menu.Item
                 name='blog'
-                only='computer'
                 active={activeItem === 'blog'}
                 onClick={handleItemClick}
               />
               <Menu.Item
                 name='contact'
-                only='computer'
                 active={activeItem === 'contact'}
                 onClick={handleItemClick}
               />
