68f16ca140db69b5232cefa5179db9f3a588c5b4
conniekang <hqk1121@gmail.com>
2021-12-09 12:15:43 -0500

    add one more project

--- src/components/Projectdata.js
+++ src/components/Projectdata.js
@@ -1,6 +1,16 @@
 export const projects = [
     {
         id: 0,
+        title: "Tab-Tab-Tab (Bill Splitting Tool)",
+        image1: "https://i.ibb.co/LQN6pXg/Hnet-com-image.gif",
+        image2: "https://i.ibb.co/jVKBFvJ/Hnet-image.gif",
+        image3: "https://i.ibb.co/L91gpp2/Hnet-com-image-1.gif",
+        tags: ["Ruby on Rail", "React", "Ant Design", "API"],
+        github: "https://github.com/conniehqk/Tabs-phase4-project",
+        description: "Tab-Tab-Tab is a tool for users to easily split expenses for extended and complicated events such as parties and trips. Users are able to log in to personal account and view current running tabs and add new expenses. When ready to settle, simply click settle and you can easily see bill splitting results."
+    },
+    {
+        id: 1,
         title: "Fancy Photography",
         image1: "https://i.ibb.co/M2CSNRb/Screen-Shot-2021-12-05-at-18-46-15.png",
         image2: "https://i.ibb.co/JkN8BBd/Screen-Shot-2021-12-05-at-18-50-11.png",
@@ -10,7 +20,7 @@ export const projects = [
         description: "Fancy Photography is a portfolio website for photographer built in React. The photographer would be able to display all his/her work through an API while allowing visitor interaction features such as commenting and sending messages. The photographer can also manage the portfolio by adding new project after (fake) login."
     },
     {
-        id: 1,
+        id: 2,
         title: "Vacation Planner",
         image1: "https://i.ibb.co/Cz6RPBc/Screen-Shot-2021-12-05-at-19-47-49.png",
         image2: "https://i.ibb.co/ypf5R1s/Screen-Shot-2021-12-05-at-19-48-11.png",
@@ -20,7 +30,7 @@ export const projects = [
         description: "Vacation Planner is a tool that you will need for your next trip. Not only can you create trips trip itinerary, you can also keep track of trip overall cost. This is an application built with React, Bootstrap, and Sinatra."
     },
     {
-        id: 2,
+        id: 3,
         title: "Simple Blog",
         image1: "https://i.ibb.co/vhw9R3R/Screen-Shot-2021-12-05-at-20-24-32.png",
         image2: "https://i.ibb.co/6sHfRJF/Screen-Shot-2021-12-05-at-20-22-58.png",
